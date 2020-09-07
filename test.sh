630cf3175036aaca8e0890f4c4ebdfd389071c63
Ramesh Kumar <codeofnode@gmail.com>
2020-09-07 21:14:08 +0530

    CODEOFNODE/DGRAPH-2417 adding chart tests

---
+++ test.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -e
+UNAME=`uname| tr '[:upper:]' '[:lower:]'`
+SED_SUFF=$([ "$UNAME" = "darwin" ] && echo ' ""' || echo "")
+RL_SUFF=$([ "$UNAME" = "darwin" ] && echo "" || echo " -f")
+SCRIPT_DIR=$(dirname -- "$(readlink "$RL_SUFF" "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
+
+mkdir -p $SCRIPT_DIR/testdir/bin $SCRIPT_DIR/testdir/helm
+export PATH=$SCRIPT_DIR/testdir/bin:$PATH
+TEST_REPO=chart-testing-action
+TEST_REPO_VERSION=v1.0.0
+KIND_REPO=kind-action
+KIND_REPO_VERION=v1.0.0-alpha.3
+INPUT_CONFIG=ct.yaml
+
+Setup() {
+	if [ ! -f $SCRIPT_DIR/testdir/bin/helm > /dev/null 2>&1 ]; then
+		wget https://get.helm.sh/helm-v3.0.2-$UNAME-amd64.tar.gz
+		tar -zxf helm-v3.0.2-$UNAME-amd64.tar.gz -C $SCRIPT_DIR/testdir/helm
+		rm -f helm-v3.0.2-$UNAME-amd64.tar.gz
+		cp $SCRIPT_DIR/testdir/helm/$UNAME-amd64/helm $SCRIPT_DIR/testdir/bin/helm
+	    	chmod +x $SCRIPT_DIR/testdir/bin/helm
+	fi
+	rm -f $SCRIPT_DIR/testdir/chart-changed
+	pushd $SCRIPT_DIR/testdir > /dev/null
+		local dr=$TEST_REPO
+		if [ ! -d $dr ]; then
+			git clone https://github.com/helm/$dr.git
+			pushd $dr > /dev/null
+				git checkout tags/$TEST_REPO_VERSION -b dgraph-test
+				sed -i$SED_SUFF \
+					-e 's|run_ct$|touch\ '$SCRIPT_DIR'/testdir/chart-changed;run_ct|' \
+					ct.sh
+			popd > /dev/null
+		fi
+		dr=$KIND_REPO
+		if [ ! -d $dr ]; then
+			git clone https://github.com/helm/$dr.git
+			pushd $dr > /dev/null
+				git checkout tags/$KIND_REPO_VERION -b dgraph-test
+				sed -i$SED_SUFF \
+					-e 's|/usr/local|'$SCRIPT_DIR'/testdir|' \
+					-e 's/linux/'$UNAME'/' \
+					-e 's|install_kind$|[\ !\ -f\ '$SCRIPT_DIR'/testdir/bin/kind\ ]\ \&\& install_kind|' \
+					-e 's|install_kubectl$|[\ !\ -f\ '$SCRIPT_DIR'/testdir/bin/kubectl\ ]\ \&\& install_kind|' \
+					-e 's/sudo//' \
+					kind.sh
+			popd > /dev/null
+		fi
+	popd > /dev/null
+	kind delete cluster --name chart-testing > /dev/null 2>&1 || true
+}
+
+Lint() {
+	INPUT_COMMAND=lint INPUT_CONFIG=$INPUT_CONFIG bash $SCRIPT_DIR/testdir/$TEST_REPO/main.sh
+}
+
+SetupKind() {
+	if [ -f $SCRIPT_DIR/testdir/chart-changed ]; then
+		INPUT_INSTALL_LOCAL_PATH_PROVISIONER=1 bash $SCRIPT_DIR/testdir/$KIND_REPO/main.sh
+	fi
+}
+
+InstallTest() {
+	INPUT_COMMAND=install INPUT_CONFIG=$INPUT_CONFIG bash $SCRIPT_DIR/testdir/$TEST_REPO/main.sh
+}
+
+GoTests() {
+	pushd $SCRIPT_DIR/tests > /dev/null
+	echo $PATH
+		go test -v .
+	popd > /dev/null
+}
+
+if [ ! -z "$RELEASE" ]; then
+	INPUT_CONFIG=ct-rel.yaml
+fi
+
+if [ -z "$1" ]; then
+	Setup
+	Lint
+	SetupKind
+	InstallTest
+	GoTests
+else
+	$1
+fi
---
+++ tests/template_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"testing"
+
+	appsv1 "k8s.io/api/apps/v1"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+)
+
+const (
+	Tag = "v20.07.0"
+)
+
+func TestPodTemplateRendersContainerImage(t *testing.T) {
+	// Path to the helm chart we will test
+	helmChartPath := "../charts/dgraph"
+
+	// Setup the args. For this test, we will set the following input values:
+	options := &helm.Options{
+		SetValues: map[string]string{"image.tag": Tag},
+	}
+
+	// Run RenderTemplate to render the template and capture the output.
+	output := helm.RenderTemplate(t, options, helmChartPath, "deployment", []string{"templates/alpha-statefulset.yaml"})
+
+	// Now we use kubernetes/client-go library to render the template output into the Deployment struct. This will
+	// ensure the Pod resource is rendered correctly.
+	var dep appsv1.StatefulSet
+	helm.UnmarshalK8SYaml(t, output, &dep)
+
+	// Finally, we verify the pod spec is set to the expected container image value
+	expectedContainerImage := "docker.io/dgraph/dgraph:" + Tag
+	podContainers := dep.Spec.Template.Spec.Containers
+	if podContainers[0].Image != expectedContainerImage {
+		t.Fatalf("Rendered container image (%s) is not expected (%s)", podContainers[0].Image, expectedContainerImage)
+	}
+}
